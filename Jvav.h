198bdb4f790279406f55bd13f0d9a6153ce476ab
WHEYAO <65268197+WHEYAO@users.noreply.github.com>
2020-05-13 15:45:48 +0800

    Add files via upload

---
+++ Jvav.h
@@ -0,0 +1,23 @@
+﻿typedef int     整型;
+typedef char    字符型;
+
+#define 如果     if
+#define 否则     else
+#define 返回     return
+#define 常量     const
+#define 浮点 float
+
+
+
+#define 标准输出 std::cout
+#define 行结束   std::endl
+#define 标准输入 std::cin
+#define 字符串   std::string
+#define  空    void
+#define 输出 cout
+
+#define 主程序   main
+#define 打开 using
+#define 命名空间 namespace
+#define 标准 std
+#define 整数 int
